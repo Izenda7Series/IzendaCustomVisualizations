90cd78226df5accfabc6bb3680a5a8e6546dcecf
Triet Pham <trietphamnm@gmail.com>
2018-07-30 14:35:53 -0400

    add Highchart 3D

---
+++ src/3dcommon.js
@@ -0,0 +1,6 @@
+import { getClass } from 'IzendaSynergy';
+import highchart3d from 'highcharts/highcharts-3d';
+
+const { VisualizationLibrary } = getClass('HighchartVizEngine');
+
+highchart3d(VisualizationLibrary);
