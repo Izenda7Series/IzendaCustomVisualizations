e491ec0c9beacb89294cd7a6aeedd3512e4c2848
ltmylinh2606 <ltmylinh2606@gmail.com>
2018-10-15 16:53:15 -0400

    Add google map api version config and change the way invokes callback

--- src/d3_googlemap/D3VizEngine.js
+++ src/d3_googlemap/D3VizEngine.js
@@ -59,7 +59,6 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 	drawMapByLatLngs(chartContainer, { data, fieldAlias, isShowTooltip }, fnCallback) {
 
 		const { map, bounds, inforWindow } = this.initialMap(chartContainer, data.length);
-		let latLng, marker;
 
 		//bind event on Map to close inforWindow
 		google.maps.event.addListener(map, 'click', function () {
@@ -67,25 +66,28 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 		});
 
 		//attach callback function when the map loaded.
-		google.maps.event.addListenerOnce(map, 'tilesloaded', fnCallback);
+		google.maps.event.addListener(map, 'tilesloaded', fnCallback);
 
 		data.forEach(item => {
 			//define latitude and longitude
-			latLng = new google
-				.maps
-				.LatLng(item.lat, item.lng);
+			const latLng = { lat: parseFloat(item.lat), lng: parseFloat(item.lng) };
 
+			//test for marker icon
+			const markerIcon = this.getPinStyle(item);
+			// const markerIcon = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 			//define marker
-			marker = new google
+			let marker = new google
 				.maps
 				.Marker({
 					position: latLng,
-					map: map,
-					icon: this.getPinStyle(item)
+					icon: markerIcon,
+					label: "Where are you?? My icon????"
 				});
 
 			//extend bounds
 			bounds.extend(latLng);
+
+			marker.setMap(map);
 
 			if (isShowTooltip) {
 				google.maps.event.addListener(marker, 'click', ((marker, item) => {
@@ -93,7 +95,7 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 						const inforHTML = `<p>Lat: ${item.lat}</p><p>Lng : ${item.lng}</p><p>${fieldAlias.metric}: ${item.metricText}</p>`;
 						inforWindow.setContent(inforHTML);
 						inforWindow.open(map, marker);
-					}
+					};
 				})(marker, item));
 			}
 		});
@@ -103,7 +105,6 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 			map.fitBounds(bounds);
 			//map.panToBounds(bounds);
 		}
-		fnCallback && fnCallback();
 	}
 
 	drawMapByGetGeoCode(chartContainer, { data, fieldAlias, isShowTooltip }, fnCallback) {
@@ -161,13 +162,15 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 
 			getGeoCode(item).then(result => {
 				if (result[0]) {
+					//marker's icon
+					const markerIcon = this.getPinStyle(item);
 					//define marker
 					marker = new google
 						.maps
 						.Marker({
 							position: result[0].geometry.location,
 							map: map,
-							icon: this.getPinStyle(item)
+							icon: markerIcon
 						});
 
 					//extend bounds
@@ -180,7 +183,7 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 								const inforHTML = `<p>${fieldAlias.geo}: ${item.postcode}</p><p>${fieldAlias.metric}: ${item.metricText}</p>`;
 								inforWindow.setContent(inforHTML);
 								inforWindow.open(map, marker);
-							}
+							};
 						})(marker, item));
 					}
 
@@ -202,14 +205,13 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 			fillColor: item.color || '#ff0000',
 			fillOpacity: .3,
 			scale: 15,
-			//scale: Math.pow(2, item.value) / 2,
 			strokeColor: '#fff',
 			strokeWeight: .5
 		};
 	}
 
 	initialMap(chartContainer, dataLength) {
-		let pos = {
+		const USA = {
 			lat: 37.09024,
 			lng: -95.712891
 		};
@@ -218,7 +220,7 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 			.maps
 			.Map(chartContainer, {
 				zoom: 2,
-				center: pos
+				center: USA
 			});
 
 		const bounds = new google
@@ -229,7 +231,7 @@ export default class D3GoogleMapVizEngine extends VizEngine {
 			.maps
 			.InfoWindow();
 
-		dataLength === 0 && bounds.extend(new google.maps.LatLng(pos.lat, pos.lng));
+		dataLength === 0 && bounds.extend(new google.maps.LatLng(USA.lat, USA.lng));
 
 		return { map, bounds, inforWindow };
 	}
--- src/d3_googlemap/google_api_loader/GoogleApi.js
+++ src/d3_googlemap/google_api_loader/GoogleApi.js
@@ -1,29 +1,31 @@
 export const GoogleApi = (opts) => {
-		opts = opts || {};
+	opts = opts || {};
 
-		if (!opts.hasOwnProperty('apiKey')) {
-				throw new Error('You must pass an apiKey to use GoogleApi');
-		}
+	if (!opts.hasOwnProperty('apiKey')) {
+		throw new Error('You must pass an apiKey to use GoogleApi');
+	}
 
-		const apiKey = opts.apiKey;
-		const URL = opts.url || 'https://maps.googleapis.com/maps/api/js';
-
-		const url = () => {
-				let url = URL;
-				let params = {
-						key: apiKey
-				};
-
-				let paramStr = Object
-						.keys(params)
-						.filter(k => !!params[k])
-						.map(k => `${k}=${params[k]}`)
-						.join('&');
-
-				return `${url}?${paramStr}`;
+	const version = 'v=3.34'; //version of google map api
+	const apiKey = opts.apiKey; // the API Key of google map api
+	const URL = opts.url || 'https://maps.googleapis.com/maps/api/js';
+	//build url string
+	const url = () => {
+		let url = URL;
+		let params = {
+			key: apiKey,
+			version
 		};
 
-		return url();
+		let paramStr = Object
+			.keys(params)
+			.filter(k => !!params[k])
+			.map(k => `${k}=${params[k]}`)
+			.join('&');
+
+		return `${url}?${paramStr}`;
+	};
+
+	return url();
 };
 
 export default GoogleApi;
